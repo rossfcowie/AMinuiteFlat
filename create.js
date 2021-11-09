a04515aade3d38054e8fcf46f34cfa629aa209f9
Ross Cowie <rossfcowie@gmail.com>
2021-11-09 13:14:36 +0000

    Created tests for creating a game board

--- create.js
+++ create.js
@@ -6,22 +6,19 @@ var x;
 var y;
 var board;
 
-function create(){
-x=5;
-y=5;
+function create(size){
+x=size;
+y=size;
 board = new Array(x);
-total = 50;
+let total = size*size *2;
 big=3;
-
-
-
 
 for(var i = 0; i < x; i++) {
     board[i] = new Array(y); 
     
     for(var j = 0; j < y; j++) {
-        board[i][j] = 1;//Math.floor(Math.random()* Math.min(big,total))+1; 
-        //total-= board[i,j];
+        board[i][j] = Math.floor(Math.random()* Math.min(big,total))+1; 
+       total-= board[i][j];
       }
       
   }
--- main.test.js
+++ main.test.js
@@ -6,10 +6,48 @@ Developer: Ross Cowie (Arumage)
 
 const { test, expect } = require('@jest/globals')
 const create = require('./create')
-const board =  [[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1]];
 
-
-test('creates a suitable game board when the function is called.',()=>{
-    expect(create()).toEqual(board);
+beforeEach(() => {
+    jest.spyOn(global.Math, 'random').mockReturnValue(0.5);
 });
 
+afterEach(() => {
+    jest.spyOn(global.Math, 'random').mockRestore();
+})
+
+test('creates a suitable game board when the function is called.',()=>{
+    let board =  [[2,2,2,2,2],[2,2,2,2,2],[2,2,2,2,2],[2,2,2,2,2],[2,2,2,2,2]];
+    expect(create(5)).toEqual(board);
+});
+
+test('board contains exactly 50 total points',() =>{
+let size = 5
+ let target = 50;
+ let board = create(size);
+ for(var i = 0; i < size; i++) {    
+    for(var j = 0; j < size; j++) {
+       target-= board[i][j];
+      }
+  }
+  expect(target).toBe(0);
+}
+)
+test('board maximum and minimum constraints true',() =>{
+    let max = 4;
+    let min = 1;
+    let cur = 2;
+    let board = create(5);
+    for(var i = 0; i < 5; i++) {    
+       for(var j = 0; j < 5; j++) {
+           cur = board[i][j]
+          if(max<cur){
+              max = cur;
+          }else if(cur<min){
+            min = cur;
+          }
+         }
+     }
+     expect(max).toBe(4);
+     expect(min).toBe(1);
+   }
+   )
